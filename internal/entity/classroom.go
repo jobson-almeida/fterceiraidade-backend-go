af0998f1677a100be542cca74cef699867574e3c
Jobson Almeida <jobson.ra@outlook.com>
2024-02-14 22:28:13 -0300

    creation of the constructor to classroom entity

--- internal/entity/classroom.go
+++ internal/entity/classroom.go
@@ -1,18 +1,46 @@
 package entity
 
+import (
+	"github.com/google/uuid"
+	"github.com/jobson-almeida/fterceiraidade-backend-go/util"
+)
+
 type Classroom struct {
 	Base
 	Name        string `json:"name" validate:"required" gorm:"type:varchar(255);unique"`
 	Description string `json:"description" validate:"required" gorm:"type:varchar(255)"`
-	Course      string `json:"course" validate:"required" gorm:"type:varchar(255)"`
+	Course      string `json:"course" validate:"required,uuid" gorm:"type:varchar(255)"`
 }
 
 func init() {}
 
-func NewClassroom() *Classroom {
-	return &Classroom{}
+func NewClassroom(name string, description string, course string) (*Classroom, error) {
+	classname := &Classroom{
+		Name:        name,
+		Description: description,
+		Course:      course,
+	}
+	classname.ID = uuid.New().String()
+
+	err := util.Validation(classname)
+	if err != nil {
+		return nil, err
+	}
+
+	return classname, nil
 }
 
-func UpdateClassroom() *Classroom {
-	return &Classroom{}
+func UpdateClassroom(name string, description string, course string) (*Classroom, error) {
+	classname := &Classroom{
+		Name:        name,
+		Description: description,
+		Course:      course,
+	}
+
+	err := util.Validation(classname)
+	if err != nil {
+		return nil, err
+	}
+
+	return classname, nil
 }
