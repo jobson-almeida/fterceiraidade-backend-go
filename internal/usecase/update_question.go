00cda34c49b16e4750ba00d680c41b6233571f37
Jobson Almeida <jobson.ra@outlook.com>
2024-02-15 10:38:32 -0300

    feat: apply the constructor on update questions

--- internal/usecase/update_question.go
+++ internal/usecase/update_question.go
@@ -18,15 +18,19 @@ func (u *UpdateQuestion) Execute(where dto.IDInput, data dto.UpdateQuestionInput
 	input := entity.NewInputID()
 	input.ID = where.ID
 
-	question := entity.UpdateQuestion()
-	question.Questioning = data.Questioning
-	question.Type = data.Type
-	question.Image = data.Image
-	question.Alternatives = data.Alternatives
-	question.Answer = data.Answer
-	question.Discipline = data.Discipline
+	question, err := entity.UpdateQuestion(
+		data.Questioning,
+		data.Type,
+		data.Image,
+		data.Alternatives,
+		data.Answer,
+		data.Discipline,
+	)
+	if err != nil {
+		return err
+	}
 
-	err := u.QuestionRepository.Update(input, question)
+	err = u.QuestionRepository.Update(input, question)
 	if err != nil {
 		return err
 	}
