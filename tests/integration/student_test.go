b47e3a4fed1e785eeb5aaa0a0256c0addbb94dd3
Jobson Almeida <jobson.ra@outlook.com>
2024-03-06 23:57:30 -0300

    test: create student

--- tests/integration/student_test.go
+++ tests/integration/student_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/jobson-almeida/fterceiraidade-backend-go/internal/repository"
 	"github.com/jobson-almeida/fterceiraidade-backend-go/tests/testhelpers"
 	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 	pg "gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -20,8 +21,8 @@ type StudentRepoTestSuite struct {
 	container  *testhelpers.DatabaseContainer
 	repository *repository.StudentRepository
 	ctx        context.Context
-	//student    *entity.InputID
-	//created    bool
+	student    *entity.InputID
+	created    bool
 	//showed     bool
 	//updated    bool
 }
@@ -58,6 +59,30 @@ func (suite *StudentRepoTestSuite) TearDownSuite() {
 }
 
 func (suite *StudentRepoTestSuite) AfterTest(_, _ string) {
+}
+
+func (suite *StudentRepoTestSuite) TestCreateStudent() {
+	t := suite.T()
+
+	address := entity.DetailsAddress{City: "City", State: "State", Street: "Street"}
+	student, err := entity.NewStudent(
+		"/avatar/avatar.png",
+		"Firstname",
+		"Lastname",
+		"email@email.com",
+		"+998812345678",
+		address,
+	)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	err = suite.repository.Create(student)
+	assert.NoError(t, err)
+
+	suite.student, err = entity.NewInputID(student.ID)
+	assert.NoError(t, err)
+	suite.created = true
 }
 
 func TestStudentRepoTestSuite(t *testing.T) {
