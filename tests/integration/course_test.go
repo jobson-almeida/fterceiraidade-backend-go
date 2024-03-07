35f9bedc1e258f9569c3f0e93502d253e6f53870
Jobson Almeida <jobson.ra@outlook.com>
2024-03-07 13:46:11 -0300

    test: delete course

--- tests/integration/course_test.go
+++ tests/integration/course_test.go
@@ -22,9 +22,9 @@ type CourseRepoTestSuite struct {
 	repository *repository.CourseRepository
 	ctx        context.Context
 	course     *entity.InputID
-	// created    bool
-	// showed     bool
-	// updated    bool
+	created    bool
+	showed     bool
+	updated    bool
 }
 
 func (suite *CourseRepoTestSuite) SetupSuite() {
@@ -59,6 +59,20 @@ func (suite *CourseRepoTestSuite) TearDownSuite() {
 }
 
 func (suite *CourseRepoTestSuite) AfterTest(_, _ string) {
+	if suite.created == true && suite.showed == true && suite.updated == true {
+		t := suite.T()
+		t.Run("delete course", func(t *testing.T) {
+			id, err := entity.NewInputID(suite.course.ID)
+			assert.NoError(t, err)
+
+			err = suite.repository.Delete(id)
+			assert.NoError(t, err)
+
+			currentCourse, err := suite.repository.Show(id)
+			assert.Error(t, err)
+			assert.Nil(t, currentCourse)
+		})
+	}
 }
 
 func (suite *CourseRepoTestSuite) TestCreateCourse() {
@@ -79,6 +93,7 @@ func (suite *CourseRepoTestSuite) TestCreateCourse() {
 
 	suite.course, err = entity.NewInputID(course.ID)
 	assert.NoError(t, err)
+	suite.created = true
 }
 
 func (suite *CourseRepoTestSuite) TestShowCourse() {
@@ -90,6 +105,7 @@ func (suite *CourseRepoTestSuite) TestShowCourse() {
 	assert.Equal(t, "Name", currentCourse.Name)
 	assert.Equal(t, "Description", currentCourse.Description)
 	assert.Equal(t, "/image/image.png", currentCourse.Image)
+	suite.showed = true
 }
 
 func (suite *CourseRepoTestSuite) TestUpdateCourse() {
@@ -110,6 +126,7 @@ func (suite *CourseRepoTestSuite) TestUpdateCourse() {
 	assert.NotEqual(t, "Name", newCourse.Name)
 	assert.NotEqual(t, "Description", newCourse.Description)
 	assert.NotEqual(t, "/image/image.png", newCourse.Image)
+	suite.updated = true
 }
 
 func TestCourseRepoTestSuite(t *testing.T) {
