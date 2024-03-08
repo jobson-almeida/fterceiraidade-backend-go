0c288fff07727408d9b705c0ff6b5c9a76d59409
Jobson Almeida <jobson.ra@outlook.com>
2024-03-07 23:45:11 -0300

    test: create question

--- tests/integration/question_test.go
+++ tests/integration/question_test.go
@@ -10,6 +10,8 @@ import (
 	"github.com/jobson-almeida/fterceiraidade-backend-go/internal/repository"
 	"github.com/jobson-almeida/fterceiraidade-backend-go/tests/testhelpers"
 	"github.com/joho/godotenv"
+	"github.com/lib/pq"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 	pg "gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -21,7 +23,7 @@ type QuestionRepoTestSuite struct {
 	repository *repository.QuestionRepository
 	ctx        context.Context
 	question   *entity.InputID
-	//created    bool
+	created    bool
 	//showed     bool
 	//updated    bool
 }
@@ -58,6 +60,36 @@ func (suite *QuestionRepoTestSuite) TearDownSuite() {
 }
 
 func (suite *QuestionRepoTestSuite) AfterTest(_, _ string) {
+}
+
+func (suite *QuestionRepoTestSuite) TestCreateQuestion() {
+	t := suite.T()
+
+	var str_img = "/image/image.png"
+	image := &str_img
+	var str_answer = "Answer"
+	answer := &str_answer
+	alternatives := pq.StringArray{"(a) alternative a", "(b) alternative b", "(c) alternative c"}
+
+	question, err := entity.NewQuestion(
+		"Questioning",
+		"Type",
+		image,
+		alternatives,
+		answer,
+		"Discipline",
+	)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	err = suite.repository.Create(question)
+	assert.NoError(t, err)
+	assert.NotNil(t, question)
+
+	suite.question, err = entity.NewInputID(question.ID)
+	assert.NoError(t, err)
+	suite.created = true
 }
 
 func TestQuestionRepoTestSuite(t *testing.T) {
